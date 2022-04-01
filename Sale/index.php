4467852b57855c202df0018b97e84ba2fde302f5
BunneyThiaReuth <bunneythiareuth@gmail.com>
2022-04-02 00:36:21 +0700

    main

--- Sale/index.php
+++ Sale/index.php
@@ -1,40 +1,40 @@
 <?php
 include('../libaries/auth.php');
 include('../database/db_connection.php');
-$message =-1;
-$messageDialog ="";
 if(!isLogin(1))
 {
    header("location:../login/login.php?page=login");
    exit();
 }
+date_default_timezone_set("Asia/Phnom_Penh");
+$t=strtotime("now");
+$message =-1;
+$messagedialog = "";
 if(isset($_GET['action']))
 {
 	$action = $_GET['action'];
 	switch($action)
 	{
 		case '1':
-
+			if(isset($_POST['txt_qty']) && $_POST['txt_qty'] > 0)
+			{
 				$pid = $_POST['txt_pid'];
 				$name = $_POST['txt_name'];
 				$price = $_POST['txt_price'];
 				$amount = $_POST['txt_amount'];
+				$invnum = $_POST['txt_invNum'];
 				$amount = number_format($amount,2);
-				$qty = 1;
-				$sql = "INSERT INTO `tbl_card`(`pid`, `name`, `price`, `qty`,`amount`) VALUES ('$pid','$name','$price','$qty','$amount')";
-				$runsql = mysqli_query($conn,$sql);
-				if($runsql)
-				{
-					$message = 1;
-					$messageDialog ="Add to card is successfully...";
-				}
-				else
-				{
-					$message = 0;
-					$messageDialog ="Add to card is not successfully !";
-				}
-				
-			
+				$qty = $_POST['txt_qty'];
+
+					$sql = "INSERT INTO `tbl_card`(`pid`, `name`, `price`, `qty`,`amount`,`ivnum`) VALUES ('$pid','$name','$price','$qty','$amount','$invnum')";
+					$runsql = mysqli_query($conn,$sql);
+
+			}
+			else
+			{
+				$message =0;
+				$messagedialog = "Quantity invalid !";
+			}
 			break;
 		case '2':
 			if(isset($_GET['id']))
@@ -42,17 +42,82 @@ if(isset($_GET['action']))
 				$id = $_GET['id'];
 				$sql ="DELETE FROM `tbl_card` WHERE `id`=$id";
 				$runsql = mysqli_query($conn,$sql);
+			}
+			break;
+		case '3':
+				if(isset($_GET['id']))
+				{
+					$id = $_GET['id'];
+					$qty = $_POST['txt_upaty'];
+					$qtyapp = $_POST['txt_qtyapp'];
+					if($qty != $qtyapp )
+					{
+						$sql = "UPDATE `tbl_card` SET `qty`='$qty' WHERE `id` = $id";
+						$runsql = mysqli_query($conn,$sql);
+						if($runsql)
+						{
+							$message =1;
+							$messagedialog = "Quantity updated is successfully...";
+						}
+						else
+						{
+							$message =0;
+							$messagedialog = "Quantity updated is not successfully !";
+						}
+					}
+					else{
+						$message =0;
+						$messagedialog = "Quantity updated is not change !";
+					}
+					
+				}
+			break;
+			
+		case '4':
+			$date = date("Y-m-d");
+			$invNumber = $t;
+			$user = $_SESSION['userID'];
+			$not = $_POST['txt_not'];
+			$sql = "INSERT INTO `tbl_invoice`(`invNumber`, `Date`, `userID`, `status`, `not`) VALUES ('$invNumber','$date','$user','0','$not')";
+			$runsql = mysqli_query($conn,$sql);
+			if($runsql){
+				$message =1;
+				$messagedialog = "Invoice created is successfully...";
+			}
+			else
+			{
+				$message =0;
+				$messagedialog = "Invoice created is not successfully...";
+			}
+			break;
+		case '5':
+				$sql = 'INSERT INTO `tbl_invoicedetail`(`invNumber`, `proID`, `price`, `qty`, `amount`)
+				SELECT `ivnum`,`pid`, `price`, `qty`, `amount`*`qty` AS "amount"
+				FROM `tbl_card`;';
+				$runsql = mysqli_query($conn,$sql);
 				if($runsql)
 				{
-					$message = 1;
-					$messageDialog ="Deleted is successfully...";
+					$message =1;
+					$messagedialog = "Submit is successfully....";
+					$deleteCard = "DELETE FROM tbl_card;";
+					mysqli_query($conn,$deleteCard);
+					if(isset($_POST['txt_upinvNum']) && $_POST['txt_upinvNum'] !="")
+					{
+						$invnumberup = $_POST['txt_upinvNum'];
+						$updateInv = "UPDATE `tbl_invoice` SET `status`='1' WHERE `invNumber` = $invnumberup";
+						mysqli_query($conn,$updateInv);
+					}
+					else
+					{
+						$message =0;
+						$messagedialog = "Submit is not successfully !";
+					}
+					
 				}
-				else
-				{
-					$message = 0;
-					$messageDialog ="Deleted is not successfully !";
+				else{
+					$message =0;
+					$messagedialog = "Submit is not successfully !";
 				}
-			}
 			break;
 	}
 }
@@ -64,34 +129,38 @@ if(isset($_GET['action']))
 </head>
 <body>
 <?php include('include/nav.php')?>
-<div class="container-fluid" style="margin-top:80px">
-	<div class="float-start" style="width: 68%;height: 100%">
+<div class="container-fluid" style="margin-top:90px">
+
+	<div class="float-end" style="width: 58%;height: 100%">
 	<div class="mt-3">
-		<?php
-			if($message == 1)
-			{
-		?>
+	<?php
+		if($message == 1)
+		{
+	?>
 		<div class="alert alert-success alert-dismissible fade show" role="alert">
-		  <strong>Messase !</strong> <?=$messageDialog?>
-		  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<strong>Success !</strong> <?=$messagedialog?>
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
-		  </button>
+		</button>
 		</div>
-		<?php
-			}
-			elseif($message ==0)
-			{
-		?>
+
+	<?php
+		}
+		elseif($message ==0)
+		{
+	?>
 		<div class="alert alert-danger alert-dismissible fade show" role="alert">
-		 <strong>Messase !</strong> <?=$messageDialog?>
-		  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<strong>Error !</strong> <?=$messagedialog?>
+		</br>
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
-		  </button>
+		</button>
 		</div>
-		<?php
-			}
-		?>
-		<table id="prolist" class="table table-striped table-hover" style="width:100%">
+
+	<?php
+		}
+	?>
+	<table id="prolist" class="table table-striped table-hover" style="width:100%">
         <thead>
             <tr class="text-center">
                 <th>Image</th>
@@ -99,8 +168,8 @@ if(isset($_GET['action']))
 				<th>Action</th>
             </tr>
         </thead>
-        <tbody>
-			<?php
+        <tbody >
+		<?php
 							$sql = 'SELECT tbl_products.pro_id AS "pid", tbl_products.pro_image AS "pimg", tbl_products.pro_name AS "pnmae", tbl_category.name AS "cateName", tbl_products.pro_stock AS "pstock", tbl_products.pro_price AS "pprice",tbl_discount.discountPerent AS "pdisc",tbl_products.pro_price-(tbl_products.pro_price*tbl_discount.discountPerent/100) AS "TotalDisc", tbl_products.pro_date AS "pdate", tbl_products.pro_description As "pdesc", tbl_user.fistName As "ufname",tbl_user.lastName AS "ulname"
 							FROM tbl_products
 							INNER JOIN tbl_category ON tbl_products.cateID=tbl_category.cateID
@@ -126,6 +195,27 @@ if(isset($_GET['action']))
 					- Sale Price : $<?=number_format($rows['TotalDisc'],2)?>
 					</div>
 					<form method="post" enctype="multipart/form-data" action="index.php?page=sale&action=1">
+					<div class="row">
+						<div class="col">
+							<input type="number" name="txt_qty" class="form-control" placeholder="Quantity" required>
+						</div>
+						<div class="col">
+								<select class="form-control" name="txt_invNum" required>
+									<option value="">--select Invoice--</option>
+									<?php
+										$sqlgetinvnum = "SELECT * FROM `tbl_invoice` WHERE `status` =0 ORDER BY `invID` DESC";
+										$runsqlgetinvnum = mysqli_query($conn,$sqlgetinvnum);
+										while($getinvnum = mysqli_fetch_array($runsqlgetinvnum))
+										{
+									?>
+										<option value="<?=$getinvnum['invNumber']?>">#<?=$getinvnum['invNumber'].".(".$getinvnum['not'].")"?></option>
+									<?php
+										}
+									?>
+								</select>
+						</div>	
+					</div>
+					
 				</td>
 				<?php
 					$proid = $rows['pid'];
@@ -134,9 +224,7 @@ if(isset($_GET['action']))
 					$pdiscount = $rows['pdisc'];
 					$pamount = $rows['TotalDisc'];
 				?>
-				
 				<td class="text-center">
-					
 						<input type="hidden" name="txt_pid" value="<?=$proid?>">
 						<input type="hidden" name="txt_name" value="<?=$pname?>">
 						<input type="hidden" name="txt_price" value="<?=$pprice?>">
@@ -147,24 +235,24 @@ if(isset($_GET['action']))
 				</td>
             </tr>
 			<?php
-				
 				}
-			   
 			?>
 		</tbody>
     </table>
+	</div>
+	</div>
+	<div style="width:41%;height:100%;">
 
-	</div>
-	</div>
-	<div class="float-end mt-3" style="width:31%;height: 100%">	
-	<div class="text-center">
-		<h2><box-icon type='solid' name='cart-alt'></box-icon>Card</h2>
+	<div>
+		<h4><box-icon type='solid' name='cart-alt'></box-icon>Card List</h4>
+		
 		<div>
-			<table class="table table-hover">
-			<thead class="bg-warning">
+			<table class="table table-hover ">
+			<thead class="bg-warning text-center">
 
 				<th>Name</th>
 				<th>Price</th>
+				<th>Sale</th>
 				<th>QTY</th>
 				<th>Amount</th>
 				<th>Action</th>
@@ -172,9 +260,8 @@ if(isset($_GET['action']))
 			</thead>
 			<tbody class="table-warning">
 				<?php
-					$getCard = 'SELECT `id`, `pid`, `name`, `price`, sum(`qty`) as "qty", `amount` 
+					$getCard = 'SELECT `id`, `pid`, `name`,`price`,`amount` as "saleprice", `qty` , `amount` * `qty` AS "amount"
 					FROM `tbl_card`
-					GROUP BY `pid`
 					ORDER BY `id` DESC;';
 					$rungetCard = mysqli_query($conn,$getCard);
 					while($card = mysqli_fetch_array($rungetCard))
@@ -182,14 +269,40 @@ if(isset($_GET['action']))
 				?>
 			  <tr>
 					<td><?=$card['name']?></td>
-				  	<td>$<?=$card['price']?></td>
-				  	<td>
-						<?=$card['qty']?>
+				  	<td class="text-center">$<?=$card['price']?></td>
+				  	<td class="text-center">$<?=$card['saleprice']?></td>
+				  	<td class="text-center">
+						
+						<?php
+							if(isset($_GET['action']) && $_GET['action'] == 'edit' && $_GET['editID'] == $card['id'])
+							{
+						?>
+						<form enctype="multipart/form-data" method="post" action="index.php?page=sale&action=3&id=<?=$card['id']?>">
+							<input type="hidden" value="<?=$card['qty']?>" name="txt_qtyapp">
+							<div class="input-group mb-3">
+							  <input type="number" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2" value="<?=$card['qty']?>" name="txt_upaty" required>
+							  <div class="input-group-append">
+								<button class="btn btn-outline-secondary" type="submit">Save</button>
+							  </div>
+							</div>
+						</form>
+						<?php
+							}
+							else
+							{
+						?>
+							<?=$card['qty']?>
+						<?php
+							}
+						?>
 				  	</td>
-				  	<td><?=$card['amount']?></td>
-				<td>
+				  	<td class="text-center"><?=$card['amount']?></td>
+				<td class="text-center">
 					<a href="index.php?page=sale&action=2&id=<?=$card['id']?>">
 						<box-icon name='trash'></box-icon>
+					</a>
+					<a href="index.php?page=sale&action=edit&editID=<?=$card['id']?>">
+						<box-icon name='edit-alt'></box-icon>
 					</a>
 				</td>
 			  </tr>
@@ -201,7 +314,7 @@ if(isset($_GET['action']))
 		</div>
 	</div>
 		<?php
-			$sqlcalCard = "SELECT SUM(`price`*`qty`) AS total,SUM(`amount`) as amount FROM `tbl_card`";
+			$sqlcalCard = "SELECT SUM(`price`*`qty`) AS total,SUM(`amount`*`qty`) as amount FROM `tbl_card`";
 			$runcardsql = mysqli_query($conn,$sqlcalCard);
 			$calCard = mysqli_fetch_array($runcardsql);
 			$totalcard = $calCard['total'];
@@ -222,11 +335,154 @@ if(isset($_GET['action']))
 				<div class="col">
 					<button type="button" class="btn btn-primary w-100">Payment</button>
 				</div>
+				<div class="col">
+					<button type="button" class="btn btn-success w-100"  data-bs-toggle="modal" data-bs-target="#staticBackdrop">Invoice</button>
+				</div>
 			</div>
 		</div>
-		
+		<div class="row mt-2">
+			<div class="col">
+				<button type="button" class="btn btn-dark w-100" data-toggle="modal" data-target="#submitINVModal">Submit Invoice</button>
+			</div>
+		</div>
+	<div class="mt-4">
+		<h4>#Invoice</h4>
+		<table id="inv" class="table table-hover" style="width: 100%">
+			<thead class="bg-primary text-white">
+				<th class="text-center">#Ref.no</th>
+				<th class="text-center">Date</th>
+				<th class="text-center">User</th>
+				<th class="text-center">Status</th>
+				<th class="text-center">Note</th>
+			</thead>
+			<tbody class="table-primary">
+				<?php
+					$sqlgetinv = 'SELECT tbl_invoice.invID AS "invID", tbl_invoice.invNumber AS "invNum", tbl_invoice.Date AS "date", tbl_user.fistName AS "fuser",tbl_user.lastName AS "luser",tbl_invoice.status AS "status",tbl_invoice.not AS "not"
+					FROM tbl_invoice
+					INNER JOIN tbl_user ON tbl_invoice.userID = tbl_user.id;';
+					 $runsqlgetinv = mysqli_query($conn,$sqlgetinv);
+					 while($getinv = mysqli_fetch_array($runsqlgetinv))
+					 {
+				?>
+				<tr>
+					<td>#<?=$getinv['invNum']?></td>
+					<td><?=$getinv['date']?></td>
+					<td><?=$getinv['fuser']." ".$getinv['luser']?></td>
+					<td class="text-center">
+						<?php
+							if($getinv['status']==0)
+							{
+								
+								echo "<box-icon type='solid' name='checkbox-minus'></box-icon>";
+							}
+						 else{
+							 echo "<box-icon type='solid' name='checkbox-checked'></box-icon>";
+						 }
+						?>
+					</td>
+					<td>
+						<?=$getinv['not']?>
+					</td>
+				</tr>
+			<?php
+					 }
+				?>
+			</tbody>
+		</table>
+	</div>
+	<div class="mt-4">
+		<table id="invDetail" class="table table-hover" style="width: 100%">
+			<thead class="text-center bg-success text-white">
+				<th>Ivoice</th>
+				<th>Prodcuts</th>
+				<th>Price</th>
+				<th>QTY</th>
+				<th>Amount</th>
+			</thead>
+			<tbody class="table-success">
+				<?php
+					$sqlinvoicDetail = 'SELECT tbl_invoicedetail.invNumber AS "invNumber", tbl_products.pro_name AS "proname", tbl_invoicedetail.price AS "price", tbl_invoicedetail.qty AS "qty", tbl_invoicedetail.amount AS "amount"
+					FROM `tbl_invoicedetail`
+					INNER JOIN `tbl_products` ON tbl_invoicedetail.proID = tbl_products.pro_id;';
+					$runsqlinvoicDetail = mysqli_query($conn,$sqlinvoicDetail);
+					while($getrowsIVNDetail = mysqli_fetch_array($runsqlinvoicDetail))
+					{
+				?>
+				<tr>
+					<td>#<?=$getrowsIVNDetail['invNumber']?></td>
+					<td><?=$getrowsIVNDetail['proname']?></td>
+					<td><?=$getrowsIVNDetail['price']?></td>
+					<td><?=$getrowsIVNDetail['qty']?></td>
+					<td><?=$getrowsIVNDetail['amount']?></td>
+				</tr>
+			<?php
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
 </div>
 </div>
+</div>
+<!-- Modal submit vin -->
+<div class="modal fade" id="submitINVModal" tabindex="-1" role="dialog" aria-labelledby="submitINVModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content alert-warning">
+      <div class="modal-header">
+        <h5 class="modal-title" id="submitINVModal">Confirm Message</h5>
+      </div>
+      <div class="modal-body">
+		 <form method="post" enctype="multipart/form-data" action="index.php?page=sale&action=5">
+		<select class="form-control" name="txt_upinvNum" required>
+				<option value="">--select Invoice--</option>
+					<?php
+						$sqlgetinvnumup = "SELECT * FROM `tbl_invoice` WHERE `status` =0 ORDER BY `invID` DESC";
+						$runsqlgetinvnumup = mysqli_query($conn,$sqlgetinvnumup);
+						while($getinvnumup = mysqli_fetch_array($runsqlgetinvnumup))
+						{
+						?>
+						<option value="<?=$getinvnumup['invNumber']?>">#<?=$getinvnumup['invNumber'].".(".$getinvnumup['not'].")"?></option>
+						<?php
+						}
+						?>
+			</select>
+			 <div class="modal-footer">
+		 		<button type="submit" class="btn btn-primary w-25">Submit</button>
+        		<button type="button" class="btn btn-secondary w-25" data-dismiss="modal">Close</button>
+      		</div>
+		 </form>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<!-- Modal INV -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm modal-center">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">CREATE INVOICE</h5>
+      </div>
+	
+      <div class="modal-body p-2">
+	  <form method="post" enctype="multipart/form-data" action="index.php?page=sale&action=4">
+		<label class="form-lable">Date :</label>
+        <input type="text" class="form-control" value="<?=date("Y-m-d")?>" name="txt_date" disabled>
+		<label class="form-lable mt-2">User :</label>
+        <input type="text" class="form-control" value="<?=$_SESSION['username']?>" name="txt_user" disabled>
+		<label class="form-lable mt-2">Invoice Number # :</label>
+        <input type="text" class="form-control" value="#<?=$t?>" name="txt_inv" disabled>
+		<label class="form-lable mt-2">Note :</label>
+        <input type="text" class="form-control" name="txt_not" required>
+		<div class="modal-footer">
+	  	<button type="submut" class="btn btn-primary w-25">Save</button>
+        <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">Close</button>
+      </div>
+	  </form>
+      </div>
+    </div>
+  </div>
 </div>
 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
@@ -237,6 +493,16 @@ if(isset($_GET['action']))
 <script>
 $(document).ready(function() {
     $('#prolist').DataTable();
+} );
+</script>
+<script>
+$(document).ready(function() {
+    $('#inv').DataTable();
+} );
+</script>
+<script>
+$(document).ready(function() {
+    $('#invDetail').DataTable();
 } );
 </script>
 <script>
--- database/liquor_store.sql
+++ database/liquor_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 29, 2022 at 06:18 PM
+-- Generation Time: Apr 01, 2022 at 05:35 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -35,8 +35,9 @@ CREATE TABLE IF NOT EXISTS `tbl_card` (
   `price` decimal(10,2) DEFAULT NULL,
   `qty` int DEFAULT NULL,
   `amount` decimal(10,2) DEFAULT NULL,
+  `ivnum` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=143 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -131,6 +132,67 @@ INSERT INTO `tbl_import` (`impID`, `date`, `pid`, `qty`, `userid`, `desc`) VALUE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_invoice`
+--
+
+DROP TABLE IF EXISTS `tbl_invoice`;
+CREATE TABLE IF NOT EXISTS `tbl_invoice` (
+  `invID` int NOT NULL AUTO_INCREMENT,
+  `invNumber` int NOT NULL,
+  `Date` date NOT NULL,
+  `userID` int NOT NULL,
+  `status` int NOT NULL,
+  `not` varchar(100) NOT NULL,
+  PRIMARY KEY (`invID`),
+  UNIQUE KEY `invNumber` (`invNumber`),
+  KEY `userInv_FK` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_invoice`
+--
+
+INSERT INTO `tbl_invoice` (`invID`, `invNumber`, `Date`, `userID`, `status`, `not`) VALUES
+(3, 1648798764, '2022-04-01', 19, 1, 'not1'),
+(7, 1648833974, '2022-04-02', 19, 1, 'not2'),
+(8, 1648834264, '2022-04-02', 19, 1, 'not3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoicedetail`
+--
+
+DROP TABLE IF EXISTS `tbl_invoicedetail`;
+CREATE TABLE IF NOT EXISTS `tbl_invoicedetail` (
+  `invDetailID` int NOT NULL AUTO_INCREMENT,
+  `invNumber` int DEFAULT NULL,
+  `proID` int DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `qty` int DEFAULT NULL,
+  `amount` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`invDetailID`),
+  KEY `proID_FK` (`proID`),
+  KEY `invumber_FK` (`invNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_invoicedetail`
+--
+
+INSERT INTO `tbl_invoicedetail` (`invDetailID`, `invNumber`, `proID`, `price`, `qty`, `amount`) VALUES
+(19, 1648798764, 2, '60.00', 2, '120.00'),
+(20, 1648798764, 3, '29.00', 2, '58.00'),
+(22, 1648833974, 2, '60.00', 2, '120.00'),
+(23, 1648833974, 5, '18.00', 2, '30.60'),
+(24, 1648833974, 10, '13.00', 4, '52.00'),
+(25, 1648834264, 13, '29.00', 3, '78.30'),
+(26, 1648834264, 3, '29.00', 3, '87.00'),
+(27, 1648834264, 5, '18.00', 3, '45.90');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_products`
 --
 
@@ -150,7 +212,7 @@ CREATE TABLE IF NOT EXISTS `tbl_products` (
   KEY `cate_FK` (`cateID`),
   KEY `discount_FK` (`pro_discount`),
   KEY `user_FK` (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_products`
@@ -165,7 +227,9 @@ INSERT INTO `tbl_products` (`pro_id`, `pro_image`, `pro_name`, `cateID`, `pro_st
 (7, '1648221384product660_2 (1).jpg', 'Beringer - Main & Vine', 35, 22, '12.90', 3, '2022-03-25', 'Manila Wine\r\nBeringer - Main & Vine', 46),
 (9, '1648221525702812367692600-A.jpg', 'Beachfront White', 35, 10, '10.00', 1, '2022-03-25', 'ALDI UK\r\nBeachfront White', 46),
 (10, '1648221577Italian-Zinfandel-A.jpg', 'Italian Zinfandel', 35, 12, '13.00', 2, '2022-03-25', 'ALDI UK\r\nItalian Zinfandel', 46),
-(11, '1648221648asc.jpg', 'Hilmar Springs Zinfandel', 35, 10, '12.00', 1, '2022-03-25', 'Drink Supermarket Hilmar Springs Zinfandel', 46);
+(11, '1648221648asc.jpg', 'Hilmar Springs Zinfandel', 35, 10, '12.00', 1, '2022-03-25', 'Drink Supermarket Hilmar Springs Zinfandel', 46),
+(12, '1648743861scarlett-dark-red-wine-blend-xl-blog0316.jpg', 'Their Sweet Red Wine Blends', 108, 12, '19.00', 1, '2022-03-31', 'Americans Sure Do Love Their Sweet Red Wine Blends | Food & Wine', 46),
+(13, '1648743945U0e4624d0f13b4f7bb67916b643709b9eO.jpg', 'Italian Red Wine Merlot 750 Ml', 108, 21, '29.00', 1, '2022-03-31', 'Alibaba\r\nItalian Red Wine Merlot 750 Ml', 46);
 
 -- --------------------------------------------------------
 
@@ -213,6 +277,19 @@ INSERT INTO `tbl_user` (`id`, `image`, `fistName`, `lastName`, `dob`, `gender`, 
 ALTER TABLE `tbl_import`
   ADD CONSTRAINT `pid_FK` FOREIGN KEY (`pid`) REFERENCES `tbl_products` (`pro_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `userId_FK` FOREIGN KEY (`userid`) REFERENCES `tbl_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  ADD CONSTRAINT `userInv_FK` FOREIGN KEY (`userID`) REFERENCES `tbl_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tbl_invoicedetail`
+--
+ALTER TABLE `tbl_invoicedetail`
+  ADD CONSTRAINT `invumber_FK` FOREIGN KEY (`invNumber`) REFERENCES `tbl_invoice` (`invNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `proID_FK` FOREIGN KEY (`proID`) REFERENCES `tbl_products` (`pro_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `tbl_products`
